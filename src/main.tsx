dd99be49a5c45ea9448d63b91e32e4c333db8a9d
UncknownGuy <cocmaster748596@gmail.com>
2024-08-20 19:21:56 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import Img from './App.jsx'
 import './index.css'
-
-createRoot(document.getElementById('root')!).render(
+import 'bootstrap/dist/css/bootstrap.min.css';
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Img />
   </StrictMode>,
 )
